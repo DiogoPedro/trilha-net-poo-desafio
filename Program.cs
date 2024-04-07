4b5a2fe840929c3518edbb47ebd6fc05e4754577
Diogo França <diogo.franca@vtb.solutions>
2024-04-07 14:30:47 -0300

    Finalizacao da atividade.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            //Create uma brake line
+            System.Console.WriteLine();
+
+            Iphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
